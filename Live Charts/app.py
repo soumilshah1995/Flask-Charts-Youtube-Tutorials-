c5d91ecf61b25470051297ad8498773752500c85
soumilshah1995 <soushah@my.bridgeport.edu>
2019-12-17 23:28:53 -0500

    added

---
+++ Live Charts/app.py
@@ -0,0 +1,24 @@
+from flask import Flask,render_template,url_for,request,redirect, make_response
+import random
+import json
+from time import time
+from random import random
+from flask import Flask, render_template, make_response
+app = Flask(__name__)
+
+
+@app.route('/', methods=["GET", "POST"])
+def main():
+    return render_template('index.html')
+
+
+@app.route('/data', methods=["GET", "POST"])
+def data():
+    data = [time() * 1000, random() * 100]
+    response = make_response(json.dumps(data))
+    response.content_type = 'application/json'
+    return response
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
