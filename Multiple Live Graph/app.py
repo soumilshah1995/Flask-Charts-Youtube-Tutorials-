de3bd192385a90dfff1e3d2c974036b63f127fa4
soumilshah1995 <soushah@my.bridgeport.edu>
2019-12-20 21:06:22 -0500

    added

---
+++ Multiple Live Graph/app.py
@@ -0,0 +1,33 @@
+from flask import Flask,render_template,url_for,request,redirect, make_response
+import random
+import json
+from time import time
+from random import random
+from flask import Flask, render_template, make_response
+app = Flask(__name__)
+
+
+@app.route('/', methods=["GET", "POST"])
+def main():
+    return render_template('index.html')
+
+
+@app.route('/data', methods=["GET", "POST"])
+def data():
+    # Data Format
+    # [TIME, Temperature, Humidity]
+
+    Temperature = random() * 100
+    Humidity = random() * 55
+
+    data = [time() * 1000, Temperature, Humidity]
+
+    response = make_response(json.dumps(data))
+
+    response.content_type = 'application/json'
+
+    return response
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
